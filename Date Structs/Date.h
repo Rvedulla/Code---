7f74c51970a47d4175e3150a7e9b6b624a395106
Rvedulla <122700554+Rvedulla@users.noreply.github.com>
2023-01-16 22:03:29 -0800

    Create Date.h

---
+++ Date Structs/Date.h
@@ -0,0 +1,31 @@
+#include <iostream> 
+#include <iomanip>
+
+using namespace std;
+
+struct Date{
+    int month;
+    int day;
+    int year;
+
+    Date(){
+    month = 1;
+    day = 1;
+    year = 1970;
+    }
+    
+    Date(int m, int d, int y){
+    month = m;
+    day = d;
+    year = y;
+
+    }
+
+};
+
+
+ostream& operator <<(ostream& os, const Date& dt){
+    os << setw(2) << setfill('0') << dt.month << "/" << setw(2) << setfill('0') << dt.day << '/' << dt.year;
+    
+    return os;
+}
